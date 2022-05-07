583863b117c298534583655fe119754ceb4bc634
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-05-07 18:45:05 -0400

    update level names

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import GameOver from "./scenes/gameOver";
 const game = new Phaser.Game(gameConfig)
 
 game.scene.add('StartScreen', StartScreen, true)
-game.scene.add('Level1', new Level(1, 3500), false)
+game.scene.add('1', new Level(1, 3500), false)
 game.scene.add('1_End', new LevelEndScreen(1), false)
-game.scene.add('Level2', new Level(2, 3000), false)
+game.scene.add('2', new Level(2, 3000), false)
 game.scene.add('2_End', new LevelEndScreen(2), false)
-game.scene.add('Level3', new Level(3, 2000), false)
+game.scene.add('3', new Level(3, 2000), false)
 game.scene.add('GameOver', GameOver, false)
